95f7ee1e43fc70da0b68e508b04776d90ba4a4e0
ltluc253 <ltluc253@gmail.com>
2017-03-31 10:25:08 +0700

    Add files via upload
    Add build script for Android

---
+++ android_build.sh
@@ -0,0 +1,303 @@
+#!/bin/bash
+#
+# build_android.sh
+# Build with NDK r10e
+
+if [ "$NDK" = "" ]; then
+	echo NDK variable not set, exiting
+	echo "Use: export NDK=/your/path/to/android-ndk"
+	exit 1
+fi
+
+OS=`uname -s | tr '[A-Z]' '[a-z]'`
+function build_x264
+{
+	PLATFORM=$NDK/platforms/$PLATFORM_VERSION/arch-$ARCH/
+	export PATH=${PATH}:$PREBUILT/bin/
+	CROSS_COMPILE=$PREBUILT/bin/$EABIARCH-
+	CFLAGS=$OPTIMIZE_CFLAGS
+	export CPPFLAGS="$CFLAGS"
+	export CFLAGS="$CFLAGS"
+	export CXXFLAGS="$CFLAGS"
+	export CXX="${CROSS_COMPILE}g++ --sysroot=$PLATFORM"
+	export AS="${CROSS_COMPILE}gcc --sysroot=$PLATFORM"
+	export CC="${CROSS_COMPILE}gcc --sysroot=$PLATFORM"
+	export NM="${CROSS_COMPILE}nm"
+	export STRIP="${CROSS_COMPILE}strip"
+	export RANLIB="${CROSS_COMPILE}ranlib"
+	export AR="${CROSS_COMPILE}ar"
+	export LDFLAGS="-Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib -lc -lm -ldl -llog"
+
+	pushd external/x264
+
+	./configure \
+        --prefix=$(pwd)/$PREFIX \
+        --host=$HOST-linux \
+        --enable-static \
+        --enable-pic \
+        --disable-asm \
+        --disable-cli \
+        $ADDITIONAL_CONFIGURE_FLAG \
+        || exit 1
+
+	make clean || exit 1
+	make -j4 install || exit 1
+	popd
+}
+
+function build_freetype2
+{
+	PLATFORM=$NDK/platforms/$PLATFORM_VERSION/arch-$ARCH/
+	export PATH=${PATH}:$PREBUILT/bin/
+	CROSS_COMPILE=$PREBUILT/bin/$EABIARCH-
+	CFLAGS=$OPTIMIZE_CFLAGS
+	export CPPFLAGS="$CFLAGS"
+	export CFLAGS="$CFLAGS"
+	export CXXFLAGS="$CFLAGS"
+	export CXX="${CROSS_COMPILE}g++ --sysroot=$PLATFORM"
+	export CC="${CROSS_COMPILE}gcc --sysroot=$PLATFORM"
+	export NM="${CROSS_COMPILE}nm"
+	export STRIP="${CROSS_COMPILE}strip"
+	export RANLIB="${CROSS_COMPILE}ranlib"
+	export AR="${CROSS_COMPILE}ar"
+	export LDFLAGS="-Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib  -nostdlib -lc -lm -ldl -llog"
+
+    pushd external/freetype2
+
+	export PKG_CONFIG_LIBDIR=$(pwd)/$PREFIX/lib/pkgconfig/
+	export PKG_CONFIG_PATH=$(pwd)/$PREFIX/lib/pkgconfig/
+	./configure \
+	    --prefix=$(pwd)/$PREFIX \
+	    --host=$HOST-linux \
+	    --disable-dependency-tracking \
+	    --disable-shared \
+	    --enable-static \
+	    --with-pic \
+	    $ADDITIONAL_CONFIGURE_FLAG \
+	    || exit 1
+
+	make clean || exit 1
+	make -j4 install || exit 1
+	popd
+}
+
+function build_ffmpeg
+{
+	PLATFORM=$NDK/platforms/$PLATFORM_VERSION/arch-$ARCH/
+	CC=$PREBUILT/bin/$EABIARCH-gcc
+	CROSS_PREFIX=$PREBUILT/bin/$EABIARCH-
+	PKG_CONFIG=${CROSS_PREFIX}pkg-config
+
+	if [ ! -f $PKG_CONFIG ];
+	then
+		cat > $PKG_CONFIG << EOF
+#!/bin/bash
+pkg-config \$*
+EOF
+		chmod u+x $PKG_CONFIG
+	fi
+
+    NM=$PREBUILT/bin/$EABIARCH-nm
+
+	pushd ~/Android/FFmpeg
+
+	export PKG_CONFIG_LIBDIR=$(pwd)/$PREFIX/lib/pkgconfig/
+	export PKG_CONFIG_PATH=$(pwd)/$PREFIX/lib/pkgconfig/
+
+	./configure --target-os=linux \
+	    --prefix=$PREFIX \
+	    --enable-cross-compile \
+	    --extra-libs="-lgcc" \
+	    --arch=$ARCH \
+	    --cc=$CC \
+	    --cross-prefix=$CROSS_PREFIX \
+	    --nm=$NM \
+	    --sysroot=$PLATFORM \
+	    --extra-cflags=" -O3 -fpic -DANDROID -DHAVE_SYS_UIO_H=1 -Dipv6mr_interface=ipv6mr_ifindex -fasm -Wno-psabi -fno-short-enums  -fno-strict-aliasing -finline-limit=300 $OPTIMIZE_CFLAGS " \
+	    --disable-shared \
+	    --enable-static \
+	    --enable-runtime-cpudetect \
+	    --extra-ldflags="-Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib  -nostdlib -lc -lm -ldl -llog -L$PREFIX/lib" \
+	    --extra-cflags="-I$PREFIX/include" \
+	    --disable-everything \
+	    --disable-libass \
+	    --disable-libvo-aacenc \
+	    --disable-libvo-amrwbenc \
+	    --enable-hwaccel=h264_vaapi \
+	    --enable-hwaccel=h264_vaapi \
+	    --enable-hwaccel=h264_dxva2 \
+	    --enable-hwaccel=mpeg4_vaapi \
+	    --enable-demuxer=mov \
+	    --enable-demuxer=h264 \
+	    --enable-demuxer=mpegvideo \
+	    --enable-demuxer=h263 \
+	    --enable-demuxer=mpegps \
+	    --enable-demuxer=mjpeg \
+	    --enable-demuxer=rtsp \
+	    --enable-demuxer=rtp \
+	    --enable-demuxer=hls \
+	    --enable-demuxer=matroska \
+	    --enable-muxer=rtsp \
+	    --enable-muxer=mp4 \
+	    --enable-muxer=mov \
+	    --enable-muxer=mjpeg \
+	    --enable-muxer=matroska \
+	    --enable-protocol=crypto \
+	    --enable-protocol=jni \
+	    --enable-protocol=file \
+	    --enable-protocol=rtp \
+	    --enable-protocol=tcp \
+	    --enable-protocol=udp \
+	    --enable-protocol=applehttp \
+	    --enable-protocol=hls \
+	    --enable-protocol=http \
+	    --enable-decoder=xsub \
+	    --enable-decoder=jacosub \
+	    --enable-decoder=dvdsub \
+	    --enable-decoder=dvbsub \
+	    --enable-decoder=subviewer \
+	    --enable-decoder=rawvideo \
+	    --enable-encoder=rawvideo \
+	    --enable-decoder=mjpeg \
+	    --enable-encoder=mjpeg \
+	    --enable-decoder=h263 \
+	    --enable-decoder=mpeg4 \
+	    --enable-encoder=mpeg4 \
+	    --enable-decoder=h264 \
+	    --enable-encoder=h264 \
+	    --enable-decoder=aac \
+	    --enable-encoder=aac \
+	    --enable-parser=h264 \
+	    --enable-encoder=mp2 \
+	    --enable-decoder=mp2 \
+	    --enable-encoder=libvo_amrwbenc \
+	    --enable-decoder=amrwb \
+	    --enable-muxer=mp2 \
+	    --enable-bsfs \
+	    --enable-decoders \
+	    --enable-encoders \
+	    --enable-parsers \
+	    --enable-hwaccels \
+	    --enable-muxers \
+	    --enable-avformat \
+	    --enable-avcodec \
+	    --enable-avresample \
+	    --enable-zlib \
+	    --disable-doc \
+	    --disable-ffplay \
+	    --disable-ffmpeg \
+	    --disable-ffplay \
+	    --disable-ffprobe \
+	    --disable-ffserver \
+	    --disable-avfilter \
+	    --disable-avdevice \
+	    --enable-nonfree \
+	    --enable-version3 \
+	    --enable-memalign-hack \
+	    --enable-asm \
+	    $ADDITIONAL_CONFIGURE_FLAG \
+	    || exit 1
+	make clean || exit 1
+	make -j4 install || exit 1
+
+	popd
+}
+
+function build_one {
+	pushd ~/Android/FFmpeg
+	PLATFORM=$NDK/platforms/$PLATFORM_VERSION/arch-$ARCH/
+	$PREBUILT/bin/$EABIARCH-ld -rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -L$PREFIX/lib -soname $SONAME -shared -nostdlib -z noexecstack -Bsymbolic --whole-archive --no-undefined -o $OUT_LIBRARY -lavcodec -lavformat -lavresample -lavutil -lswresample -lswscale -lc -lm -lz -ldl -llog --dynamic-linker=/system/bin/linker -zmuldefs $PREBUILT/lib/gcc/$EABIARCH/4.9/libgcc.a || exit 1
+	popd
+}
+
+#x86
+EABIARCH=i686-linux-android
+HOST=x86
+ARCH=x86
+OPTIMIZE_CFLAGS="-m32"
+PREFIX=../ffmpeg-build/x86
+OUT_LIBRARY=$PREFIX/libffmpeg.so
+ADDITIONAL_CONFIGURE_FLAG=--disable-asm
+SONAME=libffmpeg.so
+PREBUILT=$NDK/toolchains/x86-4.9/prebuilt/$OS-x86_64
+PLATFORM_VERSION=android-14
+build_ffmpeg
+build_one
+
+
+
+#arm64 v8a
+EABIARCH=aarch64-linux-android
+HOST=aarch64
+ARCH=arm64
+CPU=arm64
+OPTIMIZE_CFLAGS=
+PREFIX=../ffmpeg-build/arm64-v8a
+OUT_LIBRARY=../ffmpeg-build/arm64-v8a/libffmpeg.so
+ADDITIONAL_CONFIGURE_FLAG=--enable-neon
+SONAME=libffmpeg.so
+PREBUILT=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/$OS-x86_64
+PLATFORM_VERSION=android-21
+build_ffmpeg
+build_one
+
+
+
+#arm v7 + neon (neon also include vfpv3-32)
+EABIARCH=arm-linux-androideabi
+HOST=arm
+ARCH=arm
+CPU=armv7-a
+OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=neon -marm -march=$CPU -mtune=cortex-a8 -mthumb -D__thumb__"
+PREFIX=../ffmpeg-build/armeabi-v7a-neon
+OUT_LIBRARY=../ffmpeg-build/armeabi-v7a-neon/libffmpeg.so
+ADDITIONAL_CONFIGURE_FLAG=--enable-neon
+SONAME=libffmpeg-neon.so
+PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/$OS-x86_64
+PLATFORM_VERSION=android-14
+build_ffmpeg
+build_one
+
+#arm v7vfpv3
+#EABIARCH=arm-linux-androideabi
+#HOST=arm
+#ARCH=arm
+#CPU=armv7-a
+#OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfpv3-d16 -marm -march=$CPU "
+#PREFIX=../ffmpeg-build/armeabi-v7a
+#OUT_LIBRARY=$PREFIX/libffmpeg.so
+#ADDITIONAL_CONFIGURE_FLAG=
+#SONAME=libffmpeg.so
+#PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/$OS-x86_64
+#PLATFORM_VERSION=android-14
+#build_ffmpeg
+#build_one
+
+#arm v5
+#EABIARCH=arm-linux-androideabi
+#HOST=arm
+#ARCH=arm
+#CPU=armv5
+#OPTIMIZE_CFLAGS="-marm -march=$CPU"
+#PREFIX=../ffmpeg-build/armeabi
+#OUT_LIBRARY=$PREFIX/libffmpeg.so
+#ADDITIONAL_CONFIGURE_FLAG=
+#SONAME=libffmpeg.so
+#PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/$OS-x86_64
+#PLATFORM_VERSION=android-14
+#build_ffmpeg
+#build_one
+
+#mips
+#EABIARCH=mipsel-linux-android
+#HOST=mips
+#ARCH=mips
+#OPTIMIZE_CFLAGS=
+#PREFIX=../ffmpeg-build/mips
+#OUT_LIBRARY=$PREFIX/libffmpeg.so
+#ADDITIONAL_CONFIGURE_FLAG="--disable-mipsfpu --disable-asm"
+#SONAME=libffmpeg.so
+#PREBUILT=$NDK/toolchains/mipsel-linux-android-4.9/prebuilt/$OS-x86_64
+#PLATFORM_VERSION=android-14
+#build_ffmpeg
+#build_one
